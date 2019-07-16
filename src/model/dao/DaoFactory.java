861decf44722151de2e5cd7602fe4a3f72689416
Gustavo Ferreira <gu-ferreira@usp.br>
2019-07-16 19:06:44 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //classe auxiliar que instancia os DAOs
@@ -11,5 +12,7 @@ public class DaoFactory {
 	public static SellerDao createSellerDao(){
 		return new SellerDaoJDBC(DB.getConnection());
 	}
-	
+	public static DepartmentDao createDepartmentDao(){
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
