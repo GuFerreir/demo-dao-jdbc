78fc3bcab406816ee68b1047c08b077952c0fc50
Gustavo Ferreira <gu-ferreira@usp.br>
2019-07-16 00:13:36 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -66,7 +66,7 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller seller) {
-PreparedStatement st = null;
+		PreparedStatement st = null;
 		
 		try{
 			st = conn.prepareStatement(
